bf3e6029c0766d47b2bfbc8bcb5aa402d6027308
Brandon <b.c.dunlap@outlook.com>
2016-09-14 22:49:01 -0400

    Player Types
    Can now change player control scheme to facilitate, a single or two
    player game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour {
     public float jumpHeight; 
     public Text countText;
     public Text winText;
+    public bool gameStart;
+    public int playerType; 
 
     private Rigidbody rb;
     private int count;
@@ -23,25 +25,115 @@ public class PlayerController : MonoBehaviour {
     }
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        shouldJump = true;
-        if (this.gameObject.transform.position.y > 0.5)
+        if (playerType == 0)
         {
-            shouldJump = false; 
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            shouldJump = true;
+            if (this.gameObject.transform.position.y > 0.5)
+            {
+                shouldJump = false;
+            }
+
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            Vector3 jump = new Vector3(0, 1, 0);
+
+            rb.AddForce(movement * speed);
+
+
+            if (Input.GetKeyDown("space") && shouldJump)
+            {
+                rb.AddForce(jump * jumpHeight);
+            }
         }
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        Vector3 jump = new Vector3(0,1,0);
-
-        rb.AddForce(movement * speed);
-
-        
-        if (Input.GetKeyDown("space") && shouldJump)
+        if (playerType == 1)
         {
-            rb.AddForce(jump * jumpHeight);
+            float moveHorizontal = 0;
+            float moveVertical = 0;
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                moveHorizontal = 1;
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                moveHorizontal = -1;
+            }
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                moveVertical = 1;
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                moveVertical = -1;
+            }
+
+            shouldJump = true;
+            if (this.gameObject.transform.position.y > 0.5)
+            {
+                shouldJump = false;
+            }
+
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            Vector3 jump = new Vector3(0, 1, 0);
+
+            rb.AddForce(movement * speed);
+
+
+            if (Input.GetKeyDown(KeyCode.RightShift) && shouldJump)
+            {
+                rb.AddForce(jump * jumpHeight);
+            }
         }
+
+        if (playerType == 2)
+        {
+            float moveHorizontal = 0;
+            float moveVertical = 0;
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                moveHorizontal = 1;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                moveHorizontal = -1;
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                moveVertical = 1;
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                moveVertical = -1;
+            }
+
+            shouldJump = true;
+            if (this.gameObject.transform.position.y > 0.5)
+            {
+                shouldJump = false;
+            }
+
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            Vector3 jump = new Vector3(0, 1, 0);
+
+            rb.AddForce(movement * speed);
+
+
+            if (Input.GetKeyDown(KeyCode.LeftShift) && shouldJump)
+            {
+                rb.AddForce(jump * jumpHeight);
+            }
+        }
+
     }
 
     void OnTriggerEnter(Collider other)
